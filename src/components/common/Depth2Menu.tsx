c7771acd3d36551cad28004701141ff239413b61
MinJae Kim <skwze@naver.com>
2024-10-24 18:54:48 +0900

    KMJ- Aside Navigation Component (2024-10-24) AsideMenuComponent.tsx <- 메뉴 설정 객체 처리 Depth1Menu.tsx <- 메인 이름만 받아서 2depth로 나머지 정보 넘겨줌 Depth2Menu.tsx <- 나머지 객체정보를 활용해 링크밑 메뉴이름 처리 MainLayout.tsx <- 기존있던 네비부분 삭제

--- src/components/common/Depth2Menu.tsx
+++ src/components/common/Depth2Menu.tsx
@@ -2,7 +2,6 @@ import {Link} from "react-router-dom";
 import React from "react";
 
 function Depth2Menu({subMenus, basicPath}) {
-    console.log(subMenus, basicPath);
     const menuLis = subMenus.map( (menuName,idx) => {
 
         const fullPath = basicPath + menuName.toPath
