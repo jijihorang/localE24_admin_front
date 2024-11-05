633cba37c36ddda02c5fbd2e46d5bc463bdbb323
jiho <jiho7213@gmail.com>
2024-11-05 14:45:11 +0900

    HTQ-32 CJH 제작자 관리 리스트 페이지 수정(2024-11-05 14:45) AppliersListComponent 수정

--- src/components/appliers/AppliersListComponent.tsx
+++ src/components/appliers/AppliersListComponent.tsx
@@ -11,13 +11,10 @@ function AppliersListComponent() {
                         <thead>
                         <tr>
                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
-                                입점 신청인
+                                제작자명
                             </th>
                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 upperca se tracking-wider dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
                                 신청 날짜
-                            </th>
-                            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 upperca se tracking-wider dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
-                                승인 날짜
                             </th>
                             <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
                                 Status
@@ -29,13 +26,10 @@ function AppliersListComponent() {
                         <tr>
                             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm dark:bg-gray-800 dark:border-gray-700">
                                 <Link to="/appliers/read"
-                                      className="text-gray-900 whitespace-no-wrap dark:text-gray-300">민파라다이스</Link>
+                                      className="text-gray-900 whitespace-no-wrap dark:text-gray-300">김민주</Link>
                             </td>
                             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm dark:bg-gray-800 dark:border-gray-700">
                                 <p className="text-gray-900 whitespace-no-wrap dark:text-gray-300">2024-10-22</p>
-                            </td>
-                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm dark:bg-gray-800 dark:border-gray-700">
-                                <p className="text-gray-900 whitespace-no-wrap dark:text-gray-300">2024-10-23</p>
                             </td>
                             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm dark:bg-gray-800 dark:border-gray-700">
                                 <div className="flex items-center space-x-4">
@@ -44,7 +38,7 @@ function AppliersListComponent() {
                                     >
                                             <span aria-hidden
                                                   className="absolute inset-0 bg-purple-200 opacity-50 rounded-full"></span>
-                                        <span className="relative">대기중</span>
+                                        <span className="relative">승인</span>
                                     </button>
                                 </div>
                             </td>
