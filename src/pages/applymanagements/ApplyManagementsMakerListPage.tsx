9ccaa3515d02a08d287ad619094165d40e426d91
jiho <jiho7213@gmail.com>
2024-11-13 11:33:56 +0900

    test

---
+++ src/pages/applymanagements/ApplyManagementsMakerListPage.tsx
@@ -0,0 +1,15 @@
+
+import ApplyManagementsMakerListComponent
+    from "../../components/applymanagements/ApplyManagementsMakerListComponent.tsx";
+
+
+function ApplyManagementsMakerListPage() {
+    return (
+        <div className="container mx-auto px-4 sm:px-8">
+            <ApplyManagementsMakerListComponent />
+        </div>
+
+    );
+}
+
+export default ApplyManagementsMakerListPage;
